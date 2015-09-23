faaa620e06db79bfe981a2133f678e027d9812a5
dvxwang <dvxwang@gmail.com>
2015-09-23 14:07:00 -0400

    Lab 10

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -20,5 +20,31 @@
  * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
  
  */
+- (NSString *) badgeForSpeaker: (NSString *)speaker {
+    
+    NSString *result =@"";
+    result= [result stringByAppendingFormat:@"Hello, my name is %@.",speaker];
+    return result;
+}
+- (NSArray *) badgesForSpeakers: (NSArray *) speakers {
+    
+    NSMutableArray *result=[[NSMutableArray alloc] init];
+    
+    for (NSUInteger i=0;i<[speakers count];i++) {
+        [result addObject:[self badgeForSpeaker:speakers[i]]];
+    }
+    
+    return result;
+}
+- (NSArray *) greetingsAndRoomAssignmentsForSpeakers: (NSArray *) speakers {
+    
+    NSMutableArray *result=[[NSMutableArray alloc] init];
+    
+    for (NSUInteger i=0;i<[speakers count];i++) {
+        [result addObject:[NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %li.",speakers[i],i+1]];
+    }
+    
+    return result;
+}
 
 @end
