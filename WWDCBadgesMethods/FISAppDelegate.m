ccf224223cb313899612885d2aa55c62e672db38
jspringer <jsnsprngr@gmail.com>
2015-10-05 19:02:23 -0400

    Tenth

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,29 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    return [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+}
+
+- (NSMutableArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < 8; i++) {
+        NSString *speaker = speakers[i];
+        NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+        [badges addObject:badge];
+    }
+    return badges;
+}
+
+- (NSMutableArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *roomAssignments = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < 8; i++) {
+        NSString *speaker = speakers[i];
+        NSUInteger room = i + 1;
+        NSString * roomAssignment = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speaker, room];
+        [roomAssignments addObject:roomAssignment];
+    }
+    return roomAssignments;
+}
+
 @end
