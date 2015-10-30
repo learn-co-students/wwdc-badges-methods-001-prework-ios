2913ba45158a1a173b93702feb652b765596bba5
d1v1n3g0d <d1v1n3g0d@gmail.com>
2015-10-30 01:02:26 -0400

    WWDC badges methods

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,28 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSMutableString *badge = [[NSMutableString alloc] init];
+    [badge appendFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        [badges addObject:[self badgeForSpeaker:speakers[i]]];
+    }
+    return badges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *greetingAndRoomAssignment = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSMutableString *greetingAndRoom = [[NSMutableString alloc] init];
+        [greetingAndRoom appendFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], i + 1];
+        [greetingAndRoomAssignment addObject:greetingAndRoom];
+    }
+    return greetingAndRoomAssignment;
+}
+
 @end
