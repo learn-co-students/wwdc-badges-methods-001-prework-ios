9d422c4c8b7b7b0b89b73918c29a08023128082a
thejoker51 <acama23@gmail.com>
2015-10-31 13:38:56 -0400

    create three methods to satisfy tests

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,13 +12,69 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    
+//    NSArray *testSpeakers = @[ @"Jim Johnson", @"Frank Albert"];
+//    
+//    NSLog(@"This is the first greeting: %@", [self badgeForSpeaker:@"Jim John"]);
+    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *greetingText = @"Hello, my name is ";
+    
+    NSMutableString *badgeOutput =  [greetingText mutableCopy];
+    
+    [badgeOutput appendString:speaker];
+    [badgeOutput appendString:@"."];
+    
+    return badgeOutput;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *greetingForSpeakers = [NSMutableArray arrayWithArray:speakers];
+    
+    for (NSUInteger i = 0; i < [greetingForSpeakers count]; i++) {
+
+        // get the speakers name in a string
+        NSString *speakerName = speakers[i];
+//        NSLog(@"This is the speaker name: %@", speakerName);
+        
+        NSString *welcomeMessage = [self badgeForSpeaker:speakerName];
+//        NSLog(@"This is the output of the badgeForSpeaker: %@", welcomeMessage);
+        
+        [greetingForSpeakers replaceObjectAtIndex:i withObject:welcomeMessage];
+        
+    }
+    
+    return greetingForSpeakers;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+
+    NSMutableArray *greetingsAndRoomAssignment = [NSMutableArray arrayWithArray:speakers];
+    
+    for (NSUInteger i = 0; i < [greetingsAndRoomAssignment count]; i++) {
+        
+        NSString *welcomeMsg = @"Welcome, ";
+        
+        NSUInteger fixForZero = i + 1;
+        
+        NSMutableString *fullgreeting = [welcomeMsg mutableCopy];
+        
+        NSString *speakerName = greetingsAndRoomAssignment[i];
+    
+        [fullgreeting appendFormat:@"%@! You'll be in dressing room %lu.", speakerName, fixForZero];
+        
+//        NSLog(@"Output: %@", fullgreeting);
+        
+        [greetingsAndRoomAssignment replaceObjectAtIndex:i withObject:fullgreeting];
+        
+    }
+    
+    return greetingsAndRoomAssignment;
+}
 
 @end
