caf47be3f3b454700ef739d1ad7e436906569c03
dominicprescod <dominicprescod@gmail.com>
2015-11-09 13:02:54 -0500

    Weird error message, but i think i passed all tests

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,28 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *) badgeForSpeaker:(NSString *)speaker{
+    NSMutableString *badge = [[NSMutableString alloc] initWithString:@"Hello, my name is"];
+    [badge appendFormat:@" %@.",speaker];
+    return badge;
+    }
+
+- (NSArray *) badgesForSpeakers:(NSArray *)speakers{
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        badges[i] = [self badgeForSpeaker:speakers[i]];
+    }
+    return badges;
+    }
+
+- (NSArray *) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    NSMutableArray *greetings = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        [greetings[i] appendFormat:@"Welcome, %@! ",speakers[i]];
+        NSInteger j = i + 1;
+        [greetings[i] appendFormat:@"You'll be in dressing room %ld.",(long)j];
+        }
+    return greetings;
+    }
 
 @end
