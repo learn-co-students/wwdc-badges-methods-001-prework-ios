ea935ce410470f0113547d163ec7e4648961ce7f
AliMar11 <alicia.marisal@gmail.com>
2015-12-07 12:53:52 -0500

    completed badges methods lab

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,39 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *) badgeForSpeaker:(NSString *)speaker {
+    NSString *enterSpeaker = [NSString stringWithFormat: @"Hello, my name is %@.", speaker];
+    
+    return enterSpeaker;
+
+}
+
+
+- (NSArray *) badgesForSpeakers: (NSArray *) speakers {
+    NSMutableArray *allSpeakersIntro = [speakers mutableCopy];
+    NSUInteger numOfSpeakers = [speakers count];
+    for (NSUInteger i = 0; i < numOfSpeakers; i++) {
+        allSpeakersIntro[i] = [NSString stringWithFormat:@"Hello, my name is %@.", speakers[i]];
+    }
+    
+    return allSpeakersIntro;
+   
+}
+
+- (NSArray *) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *hellosAndRoomNums = [speakers mutableCopy];
+    NSUInteger numOfSpeakers = [speakers count];
+    for (NSUInteger i = 0; i < numOfSpeakers; i++) {
+        NSUInteger roomAssignment = i + 1;
+        
+    return hellosAndRoomNums[i]= [NSString stringWithFormat:@"Welcome, %@! You'll be in dressingroom %lu.", speakers[i], roomAssignment];
+        
+    }
+    
+    return hellosAndRoomNums;
+    
+}
+
+
 
 @end
