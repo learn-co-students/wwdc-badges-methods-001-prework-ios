88bb03df8feca717ca88b3e4ff25281d9af92ff8
abcheromar <abcheromar@gmail.com>
2015-12-22 03:30:12 -0500

    Done.

--- WWDCBadgesMethods/FISAppDelegate.h
+++ WWDCBadgesMethods/FISAppDelegate.h
@@ -12,10 +12,15 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/*
- 
- * Declare your custom methods in this space. Don't forget to end the statements with semicolons ";" !
- 
- */
+
+
+
+- (NSArray *)badgeForSpeaker:(NSString *)speaker;
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers;
+
+- (NSArray *) greetingsAndRoomAssignmentsForSpeakers: (NSArray *)speakers;
+
+
 
 @end
