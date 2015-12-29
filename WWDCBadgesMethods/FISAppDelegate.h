78af688106c47ebdfb651926dfa35f964533eff6
shenae <shhnae@gmail.com>
2015-12-29 05:04:19 -0500

    badges with methods still errors

--- WWDCBadgesMethods/FISAppDelegate.h
+++ WWDCBadgesMethods/FISAppDelegate.h
@@ -12,10 +12,16 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/*
- 
- * Declare your custom methods in this space. Don't forget to end the statements with semicolons ";" !
- 
- */
+-(BOOL)badgeForSpeaker:(NSString *)speaker;
+    
+
+-(BOOL)badgesForSpeakers: (NSArray *)speakers;
+
+
+-(BOOL)greetingsAndRoomAssignmentsForSpeakers: (NSArray *)speakers;
+
+
+
+
 
 @end
