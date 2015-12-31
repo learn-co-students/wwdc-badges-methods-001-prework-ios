1cf0e27cdc29bbe1de0fac6cf406c5d1e7360335
mouerj <joseph.mouer@icloud.com>
2015-12-30 22:43:20 -0500

    Complete Badges with Methods

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,34 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSString *badge = [NSString stringWithFormat: @"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSArray *speakerList = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    NSMutableArray *mspeakerList = [speakerList mutableCopy];
+    //NSString *badges = @"";
+    for (NSUInteger i = 0; i < [speakerList count]; i++) {
+        NSString *badges = [NSString stringWithFormat:@"Hello, My name is %@.", speakerList[i]];
+        [mspeakerList insertObject:badges atIndex:i];
+    }
+    return mspeakerList;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+   speakers = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    NSMutableArray *mRooms = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < [speakers count]; i++) {
+        NSUInteger room = i + 1;
+        NSString *welcomAndRoom = [NSString stringWithFormat:@"@Welcome, %@. You'll be in dressing room %lu.", speakers[i],room];
+        [mRooms insertObject:welcomAndRoom atIndex:i];
+    }
+    
+    return mRooms;
+}
+
+
+
 @end
