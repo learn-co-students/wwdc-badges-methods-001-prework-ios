e96713c417ecb9384dc888e3577efe8376ed9e9d
shibamo <qinchao@hotmail.com>
2016-01-01 17:42:05 -0500

    wrote methods to pass test specs

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,25 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString*) badgeForSpeaker:(NSString *)speaker{
+    return [NSString  stringWithFormat:@"Hello, my name is %@.",speaker];
+}
+
+- (NSArray*) badgesForSpeakers:(NSArray *)speakers{
+    NSMutableArray *a = [NSMutableArray new];
+    for(NSUInteger i=0; i<[speakers count]; i++){
+        [a addObject:[self badgeForSpeaker:speakers[i]]];
+    }
+    return a;
+}
+
+//@"Welcome, Adele Goldberg! You'll be in dressing room 1.",
+- (NSArray*) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    NSMutableArray *a = [NSMutableArray new];
+    for(NSUInteger i=0; i<[speakers count]; i++){
+        [a addObject:[NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.",speakers[i],i+1]];
+    }
+    return a;
+}
 
 @end
