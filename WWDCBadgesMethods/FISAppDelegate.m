9488aa5de89868bae9fdf962b62424a8a789d931
jen2 <sipila.jen@gmail.com>
2016-01-02 16:42:54 -0500

    Completed Badges with Methods!!
    Passed tests and did advanced challenge!!

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,46 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+-(NSString *) badgeForSpeaker:(NSString *)speaker
+{
+    
+    return [NSString stringWithFormat: @"Hello, my name is %@.", speaker];
+    
+}
+
+-(NSMutableArray *) badgesForSpeakers:(NSArray *)speakers
+{
+
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    
+    
+    for (NSUInteger i = 0; i <= [speakers count] - 1; i++)
+    
+    {
+        
+        //[badges addObject:[NSString stringWithFormat:@"Hello, my name is %@.",speakers[i]]];
+        
+        
+        [badges addObject:[self badgeForSpeaker:speakers[i]]];
+        
+    }
+    
+    return badges;
+}
+
+-(NSMutableArray *) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers
+{
+    NSMutableArray *roomAssignments = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i <= [speakers count] - 1; i++)
+        
+    {
+
+    [roomAssignments addObject:[NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.",speakers[i], i + 1]];
+    
+    }
+    
+    return roomAssignments;
+}
 
 @end
