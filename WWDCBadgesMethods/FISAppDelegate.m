2c6a5d6a06e589e746fa831840fee7ef40ad8d3f
anmolbajajnet <hello@anmolbajaj.net>
2016-01-02 20:41:17 -0800

    solve lab with help of other students pull request

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,43 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker; {
+    
+    NSString *welcomeSpeaker = [@"Hello, my name is " stringByAppendingFormat:@"%@.", speaker];
+  
+    return welcomeSpeaker;
+    
+}
+
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers; {
+    
+    NSMutableArray *individual = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        individual[i] = [self badgeForSpeaker:speaker];
+    }
+        
+    
+    return individual;
+    
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers; {
+    
+    NSMutableArray *speakerDressingRoomWelcome = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        NSString *speakerWelcome = [@"Welcome, " stringByAppendingFormat:@"%@! ", speaker];
+        NSString *speakerAndRoomNumber = [speakerWelcome stringByAppendingFormat:@"You'll be in dressing room %lu.", i+1];
+        
+        speakerDressingRoomWelcome[i] = speakerAndRoomNumber;
+        
+    }
+    
+    return speakerDressingRoomWelcome;
+}
+
+
 @end
