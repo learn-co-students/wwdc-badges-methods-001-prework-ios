9eaddd60c86499e0c611e0ef3bb50a1195e07e40
SunFloweri <irinakalashnikova@gmail.com>
2016-01-12 17:40:32 -0500

    Add new methods badgeForSpeaker, badgesForSpeakers, and greetingsAndRoomAssignmentsForSpeakers.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,13 +12,39 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    //NSArray *conferenceSpeakers = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    //for(NSInteger i = 0; i < [conferenceSpeakers count]; i++){
+    //    NSLog(@ "Hello, my name is %@.", conferenceSpeakers[i]);
+    //}
+
+    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *speakersArray = [NSMutableArray array];
+    FISAppDelegate *badge = [[FISAppDelegate alloc] init];
+    NSInteger numberOfSpeakers = 8;
+    for(NSInteger i = 0; i < numberOfSpeakers; i++){
+        NSString *badges = [badge badgeForSpeaker: speakers[i]];
+        [speakersArray addObject:badges];
+    }
+    return speakersArray;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *speakersGreetingArray = [NSMutableArray array];
+    NSInteger numberOfSpeakers = 8;
+    for(NSInteger i = 0; i < numberOfSpeakers; i++){
+        NSString *greeting = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], i+1];
+        [speakersGreetingArray addObject:greeting];
+    }
+    return speakersGreetingArray;
+}
 
 @end
