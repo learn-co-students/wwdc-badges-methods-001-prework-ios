e5ab981709737b27bb4e4f3663878fe8ff2e96bb
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-01-13 16:56:43 -0500

    double check to make sure tests work

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -11,6 +11,7 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
     
     return YES;
 }
@@ -21,4 +22,41 @@
  
  */
 
+
+
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker{
+    
+
+    NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+    
+}
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers{
+    NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < 8; i ++) {
+        NSString *speakersBadges = [NSString stringWithFormat:@"Hello, my name is %@", names[i]];
+
+        [badges addObject:speakersBadges];
+        
+        
+        
+    }
+    return badges;
+}
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSArray *roomNumbers = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8"];
+    
+    NSMutableArray *roomAssignmentsforSpeakers = [[NSMutableArray alloc] init];
+        NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *greetings = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %@.", names, roomNumbers];
+        [roomAssignmentsforSpeakers addObject:greetings];
+    }
+    
+    return roomAssignmentsforSpeakers;
+}
 @end
