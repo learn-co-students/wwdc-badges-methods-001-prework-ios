1f5444f767d42b0d9d8a4091664b41bada103cf2
Billy <billylan22@gmail.com>
2016-01-25 01:17:39 -0800

    added method for making badges and greetings

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,33 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSString *speakerBadge = @"Hello, my name is";
+    speakerBadge = [speakerBadge stringByAppendingFormat:@" %@.", speaker];
+    return speakerBadge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *speakerBadges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = [self badgeForSpeaker:speakers[i]];
+        [speakerBadges addObject:speaker];
+    }
+    return speakerBadges;
+}
+
+-(NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *speakerBadges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *welcome = @"Welcome";
+        NSString *welcomeWithName = [welcome stringByAppendingFormat:@", %@", speakers[i]];
+        NSUInteger roomNumber = i + 1;
+        NSString *roomAssignment = @"You'll be in dressing room";
+        NSString *greetingsAndRoomAssignmentsForSpeakers = [welcomeWithName stringByAppendingFormat:@"! %@ %lu.", roomAssignment, roomNumber];
+        [speakerBadges addObject:greetingsAndRoomAssignmentsForSpeakers];
+    }
+    return speakerBadges;
+
+}
+
 @end
