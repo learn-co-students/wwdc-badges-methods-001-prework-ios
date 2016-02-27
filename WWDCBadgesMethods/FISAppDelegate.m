7f58c664ec7abf9df00ba4163746a345b4740872
Dougly <dgalante11@gmail.com>
2016-02-27 17:45:01 -0500

    badgesForSpeakers test passed

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,29 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSString *hello = @"Hello, my name is";
+    NSString *helloMyNameIsSpeaker = [hello stringByAppendingFormat:@" %@.", speaker];
+    return helloMyNameIsSpeaker;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *helloMyNameIs = [[NSMutableArray alloc] init];
+    NSUInteger arrayLength = [speakers count];
+
+    for (NSUInteger i = 0; i < arrayLength; i++) {
+        
+        NSUInteger index = i;
+        NSString *speaker = [speakers objectAtIndex:index];
+        NSString *helloMyNameIsSpeaker = [self badgeForSpeaker:speaker];
+        [helloMyNameIs addObject:helloMyNameIsSpeaker];
+    }
+    
+    return helloMyNameIs;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    return nil;
+}
 @end
