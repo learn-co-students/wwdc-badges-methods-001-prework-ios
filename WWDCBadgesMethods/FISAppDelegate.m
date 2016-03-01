8db0320f09c8516c7ba221d628db8ae782ba30f3
aleclindz <alexander.v.lindsay@gmail.com>
2016-03-01 00:32:31 -0500

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,49 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *badgeText = @"Hello, my name is";
+    
+    NSMutableString *badgeTextWithName = [badgeText mutableCopy];
+    
+    [badgeTextWithName appendFormat:@" %@.", speaker];
+    
+    return badgeTextWithName;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+
+    NSArray *badgesArray = @[];
+    NSMutableArray* mutableBadgesArray = [badgesArray mutableCopy];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *badgeName = [speakers objectAtIndex:i];
+        NSString *speakerBadgeText = [self badgeForSpeaker:badgeName];
+        [mutableBadgesArray insertObject:speakerBadgeText atIndex:i];
+    }
+    
+    return mutableBadgesArray;
+    
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSArray *greetingsArray = @[];
+    NSMutableArray *mutableGreetingsArray = [greetingsArray mutableCopy];
+    NSString *greetingString = @"";
+
+
+    for (NSUInteger i = 0; i < [speakers count]; i++ ) {
+        NSMutableString *mutableGreetingString = [greetingString mutableCopy];
+        [mutableGreetingString appendFormat:@"Welcome, %@! You'll be in dressing room %u.", [speakers objectAtIndex:i], i+1];
+        [mutableGreetingsArray insertObject:mutableGreetingString atIndex:i];
+        
+    }
+    
+    
+    return mutableGreetingsArray;
+    
+}
 
 @end
