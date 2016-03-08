a7dd330fe1b0cba5e139a9291e9d216e7b27efb6
ravelasquez99 <ricky.wedew@gmail.com>
2016-03-08 11:25:06 -0800

    all tests passed

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,79 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+-(NSString *)badgeForSpeaker: (NSString *) speaker {
+    
+    
+    NSMutableString *enteredString = [speaker mutableCopy];
+    
+    NSString *returnString = [NSString stringWithFormat:@"Hello, my name is %@.", enteredString];
+    return returnString;
+}
+
+
+-(NSArray *) badgesForSpeakers: (NSArray *) speakers {
+    
+    NSMutableArray *array = [NSMutableArray new];
+    
+    
+    for (NSUInteger i = 0; i < speakers.count; i++) {
+        NSString *speaker = [speakers objectAtIndex:i];
+        
+        NSString *string = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    
+        [array addObject:string];
+        
+    }
+    
+    
+    
+    return array;
+}
+
+
+-(NSArray *) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    
+    NSMutableArray *MewTwo = [NSMutableArray new];
+    
+    for (NSUInteger i = 0; i < speakers.count; i++) {
+        NSString *speaker = [speakers objectAtIndex:i];
+        NSString *input = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speaker, i + 1];
+        
+        [MewTwo addObject:input];
+    }
+    
+    
+    return MewTwo;
+}
+
+
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
