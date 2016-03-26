f3b683c95a56d9a4b87c20a8997982726a85d729
user116796 <gyk42@hotmail.com>
2016-03-26 03:33:13 -0400

    Create WWDC Badges with Methods

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -20,5 +20,40 @@
  * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
  
  */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *speakerBadge = @"Hello, my name is";
+    speakerBadge = [speakerBadge stringByAppendingFormat:@" %@.", speaker];
+
+    return speakerBadge;
+
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *speakerBadges = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *badges = [self badgeForSpeaker:speakers[i]];
+        
+        [speakerBadges addObject:badges];
+    }
+    
+    return speakerBadges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *greetings = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSUInteger room = i + 1;
+        NSString *roomAssignmentSpeakers = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], room];
+        
+        [greetings addObject:roomAssignmentSpeakers];
+    }
+    
+    return greetings;
+}
 
 @end
