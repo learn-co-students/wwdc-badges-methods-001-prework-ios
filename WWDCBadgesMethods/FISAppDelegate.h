6e5cba2294e2d695d6cc5cdd5d3c03d0ae69c864
chriswebb09 <chris.webb5249@gmail.com>
2016-04-01 22:10:26 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.h
+++ WWDCBadgesMethods/FISAppDelegate.h
@@ -12,10 +12,10 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/*
- 
- * Declare your custom methods in this space. Don't forget to end the statements with semicolons ";" !
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker;
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers;
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers;
 
 @end
--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,38 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        NSString *badge = [self badgeForSpeaker:speaker];
+        [badges addObject:badge];
+    }
+    
+    return badges;
+}
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *badge = @"Hello, my name is ";
+    
+    return [badge stringByAppendingFormat:@"%@.",speaker];
+}
+
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *greetingsAndRooms = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        NSUInteger room = i + 1;
+        NSString *greeting = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speaker, room];
+        [greetingsAndRooms addObject:greeting];
+    }
+    
+    return greetingsAndRooms;
+}
 
 @end
