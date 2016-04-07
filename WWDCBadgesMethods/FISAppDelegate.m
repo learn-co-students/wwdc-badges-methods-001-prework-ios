5bab982453185fd0bcf01c9cc72cb6fe09466090
dpreli <dave.preli@gmail.com>
2016-04-07 13:53:53 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,13 +12,43 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    
+    
+    
+    
+    
+    NSString *badgeForSpeaker = @" ";
+- (NSString*)badgeForSpeaker:(NSString*)speaker {
+    return = nil;
+    }
+    
+    NSArray *speakers =@[ @"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"]
+    
+- (NSArray*)badgesForSpeakers:(NSArray*)speakers {
+    or (NSUInteger i = 0; i < [speakers count]; i++) {
+           NSLog(@"Hello, my name is %@.", speakers[i] );
+    }
+    
+    
+    
+    
+    
+- (NSArray*)greetingsAndRoomAssignmentsForSpeakers: (NSArray*)speakers {
+    return = nil;
+    }
+    
+
+    
+    
+    
+    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
-
+    
 @end
+
+//NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+//for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+//    NSLog(@"Hello, my name is %@.", conferenceSpeakers[i] );
+//}
