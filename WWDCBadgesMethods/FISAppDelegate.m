7fb61da4eec7944be0ddd9b5540af7f26106cda7
dpreli <dave.preli@gmail.com>
2016-04-07 14:06:07 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,43 +12,44 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    
-    
-    
-    
-    
-    NSString *badgeForSpeaker = @" ";
-- (NSString*)badgeForSpeaker:(NSString*)speaker {
-    return = nil;
-    }
-    
-    NSArray *speakers =@[ @"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"]
-    
-- (NSArray*)badgesForSpeakers:(NSArray*)speakers {
-    or (NSUInteger i = 0; i < [speakers count]; i++) {
-           NSLog(@"Hello, my name is %@.", speakers[i] );
-    }
-    
-    
-    
-    
-    
-- (NSArray*)greetingsAndRoomAssignmentsForSpeakers: (NSArray*)speakers {
-    return = nil;
-    }
-    
-
-    
-    
-    
-    
     return YES;
 }
 
-    
-@end
 
-//NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
-//for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
-//    NSLog(@"Hello, my name is %@.", conferenceSpeakers[i] );
-//}
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *allBadges = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < speakers.count; i++) {
+        NSString *currentBadge = [self badgeForSpeaker:speakers[i]];
+        [allBadges addObject:currentBadge];
+    }
+    
+    return allBadges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *allGreetings = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < speakers.count; i++) {
+        
+        NSUInteger roomNumber = i + 1;
+        
+        NSString *currentGreeting = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], roomNumber];
+        
+        [allGreetings addObject:currentGreeting];
+    }
+    
+    return allGreetings;
+}
+
+@end
