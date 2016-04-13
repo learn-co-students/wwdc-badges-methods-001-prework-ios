02a76908f8854ae06bede212cca9a53ea7452e52
dochonglo <ismaelbarry777@hotmail.com>
2016-04-12 20:02:47 -0400

    Lab 10: WWDC Badges with Methods is complete.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,53 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSMutableString *introduction = [@"Hello, my name is" mutableCopy];
+    [introduction appendFormat:@" %@.", speaker];
+    return introduction;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *badgeArray = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i=0; i<[speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        NSMutableString *introduction = [@"Hello, my name is" mutableCopy];
+        [introduction appendFormat:@" %@.", speaker];
+        [badgeArray addObject:introduction];
+    }
+    return badgeArray;
+    
+    /*
+    // Advanced Method:
+    for (NSUInteger i=0; i<[speakers count]; i++) {
+        NSString *badgeProcessing = @"";
+        badgeProcessing = speakers[i];
+        badgeProcessing = [self badgeForSpeaker:badgeProcessing];
+        [badgeArray addObject:badgeProcessing];
+    }
+    
+    return badgeArray;
+    */
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *greetingAndRoomAssignment = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i=0; i<[speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        NSMutableString *welcome = [@"Welcome," mutableCopy];
+        NSString *roomStatement = @"You'll be in dressing room";
+        [welcome appendFormat: @" %@!", speaker];
+        [welcome appendFormat: @" %@", roomStatement];
+        [welcome appendFormat: @" %lu.", i+1];
+        [greetingAndRoomAssignment addObject:welcome];
+        
+    }
+    return greetingAndRoomAssignment;
+}
+
 @end
