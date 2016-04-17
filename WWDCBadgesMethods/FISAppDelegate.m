35959442f5a312bca98c8cd458ddf95d63be0eea
hannachoi <hanna.y.choi@gmail.com>
2016-04-17 19:00:35 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,43 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker: (NSString *)speaker {
+    
+    NSMutableString *badge = [@"Hello, my name is" mutableCopy];
+    [badge appendFormat:@" %@.", speaker];
+    
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers: (NSArray *)speakers {
+    
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSString *specificSpeaker = speakers[i];
+        NSString *specificBadge = [self badgeForSpeaker:specificSpeaker];
+        [badges addObject:specificBadge];
+        
+    }
+    
+    return badges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers: (NSArray *)speakers {
+    
+    NSMutableArray *greetingsAndRoomAssignments = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSUInteger roomAssignment = i + 1;
+        NSString *specificSpeaker = speakers[i];
+        NSString *specificGreeting = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", specificSpeaker, roomAssignment];
+        [greetingsAndRoomAssignments addObject:specificGreeting];
+        
+    }
+    
+    return greetingsAndRoomAssignments;
+}
 
 @end
