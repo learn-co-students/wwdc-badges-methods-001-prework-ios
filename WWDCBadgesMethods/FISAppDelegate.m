0023459b48e7bea5f2aeb13432f7b162dd37de17
jellybeanjohnny <mamerige@icloud.com>
2016-04-26 11:24:56 -0400

    Added badge methods for lab

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,29 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker
+{
+  return [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers
+{
+  NSMutableArray *badges = [[NSMutableArray alloc] initWithCapacity:speakers.count];
+  for (NSString *speaker in speakers) {
+    [badges addObject:[self badgeForSpeaker:speaker]];
+  }
+  return badges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers
+{
+  NSMutableArray *greetingsAndRoomAssignments = [[NSMutableArray alloc] initWithCapacity:speakers.count];
+  NSUInteger index = 1;
+  for (NSString *speaker in speakers) {
+    [greetingsAndRoomAssignments addObject:[NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speaker, index]];
+    index++;
+  }
+  return greetingsAndRoomAssignments;
+}
+
 @end
