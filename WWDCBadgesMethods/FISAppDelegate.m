28deac520a11b0ebf33d79667b62c03847395c7e
mariogabriele1 <mgabriele89@gmail.com>
2016-04-28 17:48:56 -0400

    WWDC Badges with advanced section

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -1,10 +1,3 @@
-//
-//  FISAppDelegate.m
-//  WWDCBadgesMethods
-//
-//  Created by Chris Gonzales on 5/28/14.
-//  Copyright (c) 2014 FIS. All rights reserved.
-//
 
 #import "FISAppDelegate.h"
 
@@ -12,13 +5,89 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    
+
+    
+    
+    NSArray *mySpeakers = @[ @"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik" ];
+
+    
+    for (NSUInteger i = 0; i < [mySpeakers count]; i++) {
+        NSLog(mySpeakers[i]);
+    }
+    
+    
+    
+    
+    
+
+    NSMutableArray *namesForWWDC = [self badgesForSpeakers: mySpeakers];
+    
+    for (NSUInteger i = 0; i < [namesForWWDC count]; i++) {
+        NSLog(namesForWWDC[i]);
+    }
+    
+    
+    NSArray *roomNumbers = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8"];
+    
+    NSMutableArray *roomsForWWDC = [self greetingsAndRoomAssignmentsForSpeakers: mySpeakers: roomNumbers];
+    for (NSUInteger i = 0; i < [roomsForWWDC count]; i++) {
+        NSLog(roomsForWWDC[i]);
+    }
+
+    
+    
+    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+
+
+//METHODS
+
+
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+
+    NSString *returnedString = [NSString stringWithFormat:@"Hello my name is %@!", speaker];
+    
+
+    return returnedString;
+    }
+
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+
+    NSMutableArray *returnedNameArray = [NSMutableArray arrayWithCapacity: [speakers count]];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *returnedString = [NSString stringWithFormat:@"Hello my name is %@!", speakers[i]];
+        returnedNameArray[i] = returnedString;
+    }
+    
+    return returnedNameArray;
+}
+
+//New Stuff
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers :(NSArray *) rooms {
+    
+    NSMutableArray *returnedNumberArray = [NSMutableArray arrayWithCapacity: [speakers count]];
+
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *returnedString = [NSString stringWithFormat:@"Welcome %@! You'll be in dressing room %@", speakers[i], rooms[i]];
+        returnedNumberArray[i] = returnedString;
+    }
+
+    
+    
+    return returnedNumberArray;
+}
+
+
+
+
+
+
 
 @end
