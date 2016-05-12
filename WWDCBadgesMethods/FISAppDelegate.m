1475766f1537c40ce8818579896d42f4f5a3f1c0
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-05-12 13:50:25 -0400

    WWDC Badges Methods complete 2nd Passthrough

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,54 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker; {
+    
+    NSMutableString *hello = [@"Hello, my name is" mutableCopy];
+    
+    NSMutableString *mSpeaker = [speaker mutableCopy];
+    
+    [hello appendFormat:@" %@.", mSpeaker];
+    
+ //   NSLog(@"\n\n\n\n %@ %@.", hello, mSpeaker);
+    
+    return hello;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers; {
+    
+    NSMutableArray *mSpeakers = [speakers copy];
+    
+    NSMutableArray *mute = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSString *hello = [self badgeForSpeaker:mSpeakers[i]];
+        
+        [mute addObject:hello];
+
+    }
+    return mute;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers; {
+    
+    NSMutableArray *finalBadge = [[NSMutableArray alloc] init];
+//Tests require to return a Mutable Array.
+    
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+      
+        NSUInteger roomNumber = i + 1;
+//Had to add this NSUInteger, since index begins at 0, the first Room Numebr being printed originally was 0.
+        
+        NSString *finalBadges = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], roomNumber];
+//Note the change in format specifier to differentiate the speaker names from the room numbers.
+        
+        [finalBadge addObject:finalBadges];
+        
+    }
+    
+    return finalBadge;
+}
 
 @end
