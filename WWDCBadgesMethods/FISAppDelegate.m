30cc5d35d968097d764eb09fb265ff813a7832af
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-05-19 13:54:08 -0400

    WWDC Badges Methods 3rd passthrough complete

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,52 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker; {
+    
+    NSLog(@"\n\n\n\n\n%@\n\n\n\n\n\n", speaker);
+// I applied this NSLog just to see what the NSString *speaker is printing out.
+    
+   NSMutableString *badge = [@"" mutableCopy];
+    [badge appendFormat:@"Hello, my name is %@.", speaker];
+    
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers; {
+    
+    NSMutableArray *mSpeakers = [[NSMutableArray alloc] init];
+
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+         NSString *badgesSpeakers = [self badgeForSpeaker:speakers[i]];
+// Calling in the previous method in the loop to addObject: to my mutable array.
+        
+      [mSpeakers addObject:badgesSpeakers];
+        
+    }
+    
+    return mSpeakers;
+    
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers; {
+    
+ // Welcome <speaker>! You'll be in dressing room <roomNumber>.'
+    
+    NSMutableArray *speakerWithRoom = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSString *speakersWithRoomMethod = @"";
+       speakersWithRoomMethod = [speakersWithRoomMethod stringByAppendingFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], i + 1];
+// Created an empty string & utilized stringByAppendingFormat to apply the format of the sentence & ensure I could take NSString *speakersWithRoomMethod & addObject: to my mutable array.
+        
+        [speakerWithRoom addObject:speakersWithRoomMethod];
+        
+    }
+    
+    return speakerWithRoom;
+
+}
 
 @end
