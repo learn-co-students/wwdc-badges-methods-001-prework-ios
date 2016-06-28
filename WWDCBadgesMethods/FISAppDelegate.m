5277f73d82ff7db02ac8c8d76dfa7f1cec435a35
volldammd <volldammd@gmail.com>
2016-06-28 18:01:34 -0400

    revised

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -6,40 +6,76 @@
 //  Copyright (c) 2014 FIS. All rights reserved.
 //
 
+
 #import "FISAppDelegate.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    NSString *speaker = @"Inigo Montoya";
+    NSString *badge = [self badgeForSpeaker:speaker];
+    NSLog(@"%@", badge);
+    
+    
+    NSMutableArray *conferenceSpeakers = [[NSMutableArray alloc] init];
+    NSArray *hello = [self badgesForSpeakers:conferenceSpeakers];
+    NSLog(@"%@", hello);
+    
+    
+    NSMutableArray *conferenceRooms = [[NSMutableArray alloc] init];
+    NSArray *announcement = [self greetingsAndRoomAssignmentsForSpeakers: conferenceRooms];
+    NSLog(@"%@", announcement);
+    
     return YES;
 }
 
-
--(NSString*)badgeForSpeaker:(NSString*)speaker
-{
-
-
-
-    return speaker;
-}
--(NSArray*)badgesForSpeakers:(NSArray*)speakers
-{
-
-
-    return speakers;
+- (NSString *)badgeForSpeaker:(NSString *)speaker;{
+    
+    NSString *name = [speaker copy];
+    
+    NSString *greeting = [NSString stringWithFormat:@"Hello, my name is %@.", name];
+    
+    return greeting;
+    
 }
 
--(NSArray*)greetingsAndRoomAssignmentsForSpeakers:(NSArray*)speakers
-{
-
-    return speakers;
+-(NSMutableArray *)badgesForSpeakers:(NSArray *)speakers; {
+    
+    speakers = @[ @"Adele Goldberg",  @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    
+    NSMutableArray *conferenceSpeakers = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSString *greeting = [NSString stringWithFormat:@"Hello, my name is %@.", speakers[i]];
+        
+        [conferenceSpeakers insertObject:greeting atIndex:i];
+        
+    }
+    
+    return conferenceSpeakers;
+    
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+-(NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers;{
+    
+    speakers = @[ @"Adele Goldberg",  @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    
+    NSMutableArray *conferenceRooms = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSUInteger roomNumber = i + 1;
+        
+        NSString *greeting = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], roomNumber];
+        
+        [conferenceRooms insertObject:greeting atIndex:i];
+        
+    }
+    
+    return conferenceRooms;
+    
+}
 
 @end
