a310e312d295977351c95d8c4672fdff2bbbfa65
OliviaGunton <olivia.gunton@gmail.com>
2016-06-30 14:27:13 -0400

    Create badges and greetings using mutable strings and arrays

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,46 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+//Return string in format "Hello, my name is [speaker]."
+- (NSString *)badgeForSpeaker:(NSString *)speaker{
+    
+    NSMutableString *badge = [[NSMutableString alloc] init];
+    
+    [badge appendFormat:@"Hello, my name is %@.", speaker];
+    
+    return badge;
+}
+
+//Return array of badge strings for all speakers
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers{
+    
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    
+    NSUInteger numSpeakers = [speakers count];
+    
+    for (NSUInteger i = 0; i < numSpeakers; i++){
+        badges[i] = [self badgeForSpeaker:speakers[i]];
+    }
+    
+    return badges;
+}
+
+//Return string in format: "Welcome, [speaker]! You'll be in dressing room [roomNumber].
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    
+    NSMutableArray *greetings = [[NSMutableArray alloc] init];
+    
+    NSUInteger numSpeakers = [speakers count];
+    
+    for (NSUInteger i = 0; i < numSpeakers; i++){
+        NSMutableString *greeting = [[NSMutableString alloc] init];
+        
+        [greeting appendFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], i+1];
+        
+        greetings[i] = greeting;
+    }
+    
+    return greetings;
+}
 
 @end
