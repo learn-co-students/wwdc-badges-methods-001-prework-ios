e1a3121b7a94564f9d06099af2b3e5b76722f219
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2016-08-10 10:03:56 +0100

    Badges for the lads

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,13 +12,92 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+
+    
+
+    
+    
+    
+   
+    
+    
+    
+    
     return YES;
 }
+
+
+
 
 /*
  
  * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
  
  */
+
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+   
+    
+    NSMutableString * hello = [@"Hello, my name is" mutableCopy];
+    
+    [hello appendFormat:@" %@.", speaker];
+    
+    
+    return hello;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    
+        NSMutableArray *badges = [NSMutableArray array];
+        for (NSString* speaker in speakers)
+        {
+            NSString *greeting = [NSString stringWithFormat:@"Hello, my name is %@.",speaker];
+            [badges addObject:greeting];
+        }
+        return badges;
+    }
+    
+
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSArray * numbers;
+    
+    
+    numbers = [NSArray arrayWithObjects:@"1", @"2",@"3", @"4", @"5", @"6", @"7", @"8", nil];
+    
+      NSMutableArray *rooms = [NSMutableArray array];
+    
+    
+    
+    for (NSUInteger i = 0; i < 8; i++)
+        
+        
+    {
+        
+        
+        NSString *room = [NSString stringWithFormat:@"Welcome, %@! " @"You'll be in dressing room %@.",speakers[i], numbers[i]];
+        
+      
+        
+        
+        
+          [rooms addObject:room];
+
+        
+    }
+    
+    
+    
+    
+    
+    return rooms;
+}
+
+
+
+
 
 @end
