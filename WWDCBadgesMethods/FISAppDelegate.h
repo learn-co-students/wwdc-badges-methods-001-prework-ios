6b9058669610eda03cb349a9130f5fba0cf0e81f
Matt Tripodi <mtripodi1027@gmail.com>
2016-08-25 22:09:25 -0400

    WWDC Badges with Methods Lab

--- WWDCBadgesMethods/FISAppDelegate.h
+++ WWDCBadgesMethods/FISAppDelegate.h
@@ -17,5 +17,11 @@
  * Declare your custom methods in this space. Don't forget to end the statements with semicolons ";" !
  
  */
+- (NSString *)badgeForSpeaker:(NSString *)speaker;
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers;
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers;
+
 
 @end
--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,32 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speakers {
+    NSString *result = [NSString stringWithFormat:@"Hello, my name is %@." , speakers];
+    
+    return  result;
+}
+
+- (NSMutableArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *mArray = [NSMutableArray array];
+    FISAppDelegate *makeBadge = [[FISAppDelegate alloc] init];
+    
+    for (NSInteger i = 0; i < 8; i++) {
+        NSString *badge = [makeBadge badgeForSpeaker:speakers[i]];
+        [mArray addObject:badge];
+    }
+    
+    return mArray;
+}
+
+- (NSMutableArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *mArray = [NSMutableArray array];
+    
+    for (NSInteger i = 0; i < 8; i++) {
+        NSString *line = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu." , speakers[i], i + 1];
+        [mArray addObject:line];
+    }
+    
+    return mArray;
+}
 @end
