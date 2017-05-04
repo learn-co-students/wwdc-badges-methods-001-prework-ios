91eb3836d9b20e8f3750caa42c92a1cec6845d83
skakig <skakig@gmail.com>
2017-05-04 16:36:57 -0700

    My first commit for Badges-Methods

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,43 @@
     return YES;
 }
 
+
 /*
  
  * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
  
  */
+
+- (NSString*)badgeForSpeaker:(NSString *)speaker{
+    
+    NSString *badgeForSpeaker = @"Hello, my name is %@";
+    NSLog(@"%@", badgeForSpeaker); 
+
+    return badgeForSpeaker;
+}
+
+/* code block from the lesson NSArray and NSMutableArray
+ 
+ NSArray *students = @[ @"Joe", @"Mark"];
+ for (NSUInteger i = 0; i < [students count]; i++) {
+ NSLog(@"Welcome, %@!", students[i] );
+ }
+ 
+ */
+- (NSArray*)badgesForSpeakers:(NSArray *)speakers {
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSLog(@"Hello, my name is %@", speakers[i]);
+    }
+    
+    return speakers;
+}
+
+- (NSArray*)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSLog(@"Welcome, %@! You'll be in dressing room %@", speakers[i], speakers[i]);
+    }
+    
+    return speakers;
+}
 
 @end
